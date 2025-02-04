cb953632ffd809606405198b82c162e792c57bd0
Maksim Shupta <maxshupta@yahoo.com>
2025-02-04 22:41:34 +0100

    changes: error fixes

--- src/components/CommodityList.jsx
+++ src/components/CommodityList.jsx
@@ -1,17 +1,25 @@
 import { fetchGoods } from "../network";
 import CommodityCard from "./CommodityCard";
+import { useState, useEffect } from "react";
 
 let items;
 
-const CommodityList = async () => {
-  try {
-    const lstItems = await fetchGoods();
-    items = lstItems;
-    console.log("Commodity list: ", items);
-  } catch (error) {
-    console.log("Error --------- !!!!!!!!!!!!!!!!!!!!!!! ----------");
-    console.error(error);
-  }
+const CommodityList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const lstItems = await fetchGoods(); // Fetch the data
+        setItems(lstItems); // Update the state with the fetched items
+      } catch (error) {
+        console.error("Error fetching goods:", error);
+        //setError(error); // Set the error state if something goes wrong
+      }
+    };
+
+    fetchData(); // Call the async function to fetch data
+  }, []); // Empty dependency array ensures this runs only once on mount
 
   return (
     <div
@@ -19,7 +27,7 @@ const CommodityList = async () => {
       className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 max-w-7xl mx-auto px-4"
     >
       {items.map((item) => (
-        <CommodityCard key={item.id} />
+        <CommodityCard key={item.id} item={item} />
       ))}
       ;
     </div>
